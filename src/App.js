8645b34739a55d5463615a4433a00150fe1b386d
prorege <yangsehyuk1117@gmail.com>
2024-11-08 19:03:37 +0900

    oss2024-4-2 react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Component1 from "./Component1";
+import Component2 from "./Component2";
+
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is a site for oss2024-4-2react</p>
+        <Component1 />
+        <Component2 />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Component1.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Component1 extends Component {
+  render() {
+    return <div>Component1</div>;
+  }
+}
